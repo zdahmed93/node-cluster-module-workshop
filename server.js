bbb4e79c6a58a1587a7c3b4ccccbee6528ca6a1e
Ahmed ZRIBI <ahmedzribiahmed@gmail.com>
2024-06-05 00:32:28 +0200

    Add route to simulate CPU work

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/compute', (req, res) => {
+    for (let i = 0; i <= 4_000_000_000; i++) {
+        // Simulate CPU Work: a computation that takes a long time 
+    } 
+    res.send('Ok');
+})
+
 const PORT = 7000;
 
 app.listen(PORT, () => {
